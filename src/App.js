d6f3c454fa6ab142f8e56590d2fceab90d492720
tordsyabe <extordsllave@gmail.com>
2019-10-20 00:38:50 +0400

    updates on dark mode

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ function App() {
         {a.enableEditUsername ? (
           <input
             name={a.id}
-            type="text"
+            type='text'
             value={a.username}
             style={{
               borderBottom: "1px solid #cccccc",
@@ -129,7 +129,7 @@ function App() {
         {a.enableEditPassword ? (
           <input
             name={a.id}
-            type="text"
+            type='text'
             value={a.password}
             style={{
               borderBottom: "1px solid #cccccc",
@@ -163,27 +163,50 @@ function App() {
         <div
           className={isDarkMode ? darkstyles.formWrapper : styles.formWrapper}
         >
-          <h1>Label Sticker</h1>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h1>Label Sticker</h1>
+            <input
+              type='checkbox'
+              onClick={() => setIsDarkMode(!isDarkMode)}
+              style={{ margin: "10px 0", display: "none" }}
+              id='toggleDarkMode'
+            />
+            <label
+              htmlFor='toggleDarkMode'
+              style={{
+                cursor: "pointer",
+                color: "grey",
+                fontSize: "13px",
+                border: "1px solid grey",
+                borderRadius: "31px",
+                padding: "10px",
+                height: "15px"
+              }}
+            >
+              {isDarkMode ? "Light Mode" : "Dark Mode"}
+            </label>
+          </div>
+
           <form
             className={isDarkMode ? darkstyles.form : styles.form}
-            autoComplete="off"
+            autoComplete='off'
             onSubmit={handleGenerate}
           >
             <input
               className={isDarkMode ? darkstyles.input : styles.input}
               required
-              type="text"
-              name="user"
-              placeholder="Username"
+              type='text'
+              name='user'
+              placeholder='Username'
               onChange={e => setUser(e.target.value)}
             />
             <br />
             <input
               className={isDarkMode ? darkstyles.input : styles.input}
               required
-              type="text"
-              name="pass"
-              placeholder="Password"
+              type='text'
+              name='pass'
+              placeholder='Password'
               onChange={e => setPass(e.target.value)}
             />
             <br />
@@ -191,20 +214,20 @@ function App() {
             <input
               className={isDarkMode ? darkstyles.input : styles.input}
               required
-              type="number"
-              min="1"
-              max="16"
-              name="count"
-              placeholder="How many?"
+              type='number'
+              min='1'
+              max='16'
+              name='count'
+              placeholder='How many?'
               onChange={e => setCount(e.target.value)}
             />
             <br />
             <input
-              type="submit"
+              type='submit'
               className={
                 isDarkMode ? darkstyles.buttonGenerate : styles.buttonGenerate
               }
-              value="Generate"
+              value='Generate'
             />
             <button
               className={
@@ -215,6 +238,15 @@ function App() {
               Clear
             </button>
           </form>
+          <p
+            style={{
+              color: "grey",
+              marginTop: "50px",
+              fontSize: "15px"
+            }}
+          >
+            This app only supports 8 rows and 2 columns label stickers.
+          </p>
         </div>
         <div
           className={
@@ -227,21 +259,26 @@ function App() {
           >
             {users.length === 0 ? (
               <React.Fragment>
-                <img
-                  style={{
-                    width: "50%",
-                    margin: "20px"
-                  }}
-                  src={addContent}
-                  alt="add"
-                />
-                <p className={isDarkMode ? darkstyles.pTag : styles.pTag}>
-                  Get Started.
-                </p>
-                <p>
-                  Please fill in the fields on the left to start generating
-                  username and password.
-                </p>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <img
+                    style={{
+                      width: "50%",
+                      margin: "20px",
+                      alignSelf: "center"
+                    }}
+                    src={addContent}
+                    alt='add'
+                  />
+                  <div>
+                    <p className={isDarkMode ? darkstyles.pTag : styles.pTag}>
+                      Get Started.
+                    </p>
+                    <p style={{ color: "grey", fontSize: "15px" }}>
+                      Please fill in the fields on the left to start generating
+                      username and password.
+                    </p>
+                  </div>
+                </div>
               </React.Fragment>
             ) : (
               <ul className={isDarkMode ? darkstyles.lists : styles.lists}>
@@ -249,6 +286,7 @@ function App() {
               </ul>
             )}
           </div>
+
           <ReactToPrint
             trigger={() => (
               <button
@@ -257,7 +295,11 @@ function App() {
                   isDarkMode ? darkstyles.buttonPrint : styles.buttonPrint
                 }
                 style={{
-                  background: users.length === 0 ? "#cccccc" : null
+                  overflow: "hidden",
+                  position: "absolute",
+                  right: 0,
+                  bottom: users.length === 0 ? "-300px" : "0",
+                  transition: "all 400ms ease-in-out"
                 }}
               >
                 Print
