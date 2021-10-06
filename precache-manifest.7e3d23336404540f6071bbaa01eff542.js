43f3d70c0505559c728d2981af4155df96f14ea3
tordsyabe <46318298+tordsyabe@users.noreply.github.com>
2021-10-06 19:43:15 +0400

    Updates

---
+++ precache-manifest.7e3d23336404540f6071bbaa01eff542.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22f2398062f1cbe5d26063f4ea5cead",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e6d1046790ac6953d9ff",
+    "url": "/react-gh-pages/static/css/main.bcb29a26.chunk.css"
+  },
+  {
+    "revision": "abc5eaa65477c0d4f3a3",
+    "url": "/react-gh-pages/static/js/2.67baec51.chunk.js"
+  },
+  {
+    "revision": "e6d1046790ac6953d9ff",
+    "url": "/react-gh-pages/static/js/main.f1c5fdb2.chunk.js"
+  },
+  {
+    "revision": "12347391107791c2072e",
+    "url": "/react-gh-pages/static/js/runtime-main.d158ccc9.js"
+  },
+  {
+    "revision": "c593a7f44980c908773113e000f0408d",
+    "url": "/react-gh-pages/static/media/add-content.c593a7f4.svg"
+  }
+]);
